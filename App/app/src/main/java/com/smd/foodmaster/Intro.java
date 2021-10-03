e0557d507e68519a454bdd9f497221271e9f4402
sachintha00 <sachintha0603@gmail.com>
2021-10-03 19:54:55 +0530

    updated

--- App/app/src/main/java/com/smd/foodmaster/Intro.java
+++ App/app/src/main/java/com/smd/foodmaster/Intro.java
@@ -3,12 +3,23 @@ package com.smd.foodmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class Intro extends AppCompatActivity {
+
+    ImageView splashBg;
+    LottieAnimationView splashLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        splashBg = (ImageView)findViewById(R.id.splshImg);
+        splashLogo = (LottieAnimationView)findViewById(R.id.splashAnim);
+
+        splashBg.animate().translationY(-5600).setDuration(3000).setStartDelay(4000);
     }
 }
