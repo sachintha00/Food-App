cf674c51fc6f84371d8900bdc76b3697e50e811e
sachintha00 <sachintha0603@gmail.com>
2021-10-08 19:11:55 +0530

    Update Intro.java

--- App/app/src/main/java/com/smd/foodmaster/Intro.java
+++ App/app/src/main/java/com/smd/foodmaster/Intro.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
 
@@ -13,15 +16,18 @@ public class Intro extends AppCompatActivity {
 
     ImageView splashBg;
     LottieAnimationView splashLogo;
-    LinearLayout sliders;
+    LinearLayout sliders,dotLayout;
     ViewPager sliderViewPager;
     SliderAdapter sliderAdapter;
+
+    private TextView[] dots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
 
+        dotLayout = (LinearLayout)findViewById(R.id.slideDotes);
         splashBg = (ImageView)findViewById(R.id.splshImg);
         splashLogo = (LottieAnimationView)findViewById(R.id.splashAnim);
         sliders = (LinearLayout)findViewById(R.id.sliderLayout);
@@ -31,7 +37,44 @@ public class Intro extends AppCompatActivity {
         sliders.animate().setStartDelay(2700);
 
         sliderViewPager = (ViewPager)findViewById(R.id.sliders);
+
         sliderAdapter = new SliderAdapter(this);
         sliderViewPager.setAdapter(sliderAdapter);
+
+        addDotIndecator();
+    }
+
+    public void addDotIndecator(int position){
+        dots = new TextView[3];
+
+        for (int i=0; i<dots.length; i++){
+            dots[i]= new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(45);
+            dots[i].setTextColor(getResources().getColor(R.color.colorPrimary));
+
+            dotLayout.addView(dots[i]);
+        }
+
+        if(dots.length > 0){
+            dots[position].setTextColor(getResources().getColor(R.color.colorAccent));
+        }
+    }
+
+    ViewPager.OnPageChangeListener viewLister = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addDotIndecator(position);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
     }
 }
