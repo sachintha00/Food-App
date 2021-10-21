5ea67d6f2c719b9e96c9b4a06765725cc63ae193
sachintha00 <sachintha0603@gmail.com>
2021-10-21 21:44:13 +0530

    Update LoginActivity.java

--- App/app/src/main/java/com/smd/foodmaster/LoginActivity.java
+++ App/app/src/main/java/com/smd/foodmaster/LoginActivity.java
@@ -37,7 +37,7 @@ public class LoginActivity extends AppCompatActivity {
                 newMemberLayout.animate().alpha(0).setDuration(1000).setStartDelay(1000);
                 oldMemberLayout.animate().alpha(1).setDuration(1000).setStartDelay(1000);
                 loginRegBg.animate().translationY(1570).setDuration(2200).setStartDelay(200);
-                loginComponent.animate().translationY(-400).alpha(0).setDuration(800).setStartDelay(1000);
+                loginComponent.animate().translationY(-300).alpha(0).setDuration(800).setStartDelay(1000);
             }
         });
         oldMember.setOnClickListener(new View.OnClickListener() {
@@ -46,7 +46,7 @@ public class LoginActivity extends AppCompatActivity {
                 newMemberLayout.animate().alpha(1).setDuration(1000).setStartDelay(1000);
                 oldMemberLayout.animate().alpha(0).setDuration(1000).setStartDelay(1000);
                 loginRegBg.animate().translationY(-1570).setDuration(2200).setStartDelay(200);
-                loginComponent.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(1000);
+                loginComponent.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(1300);
             }
         });
     }
