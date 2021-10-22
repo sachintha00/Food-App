ec19f9fbc563e3229081f97aabc9bcc85d45bae0
sachintha00 <sachintha0603@gmail.com>
2021-10-22 12:35:46 +0530

    updated

--- App/app/src/main/java/com/smd/foodmaster/LoginActivity.java
+++ App/app/src/main/java/com/smd/foodmaster/LoginActivity.java
@@ -14,7 +14,7 @@ public class LoginActivity extends AppCompatActivity {
 
     ImageView loginRegBg;
     Animation logRegBgAnima;
-    LinearLayout newMemberLayout, loginComponent,oldMemberLayout;
+    LinearLayout newMemberLayout, loginComponent,oldMemberLayout,registerComponent;
 
     Button newMember,login, oldMember;
 
@@ -28,26 +28,37 @@ public class LoginActivity extends AppCompatActivity {
         newMemberLayout = (LinearLayout)findViewById(R.id.layout_new_member);
         oldMemberLayout = (LinearLayout)findViewById(R.id.layout_old_member);
         loginComponent = (LinearLayout)findViewById(R.id.login_component);
+        registerComponent = (LinearLayout)findViewById(R.id.register_component);
 
         loginRegBg = (ImageView) findViewById(R.id.image_bg_login_reg);
 
         newMember.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                newMemberLayout.animate().alpha(0).setDuration(1000).setStartDelay(1000);
-                oldMemberLayout.animate().alpha(1).setDuration(1000).setStartDelay(1000);
-                loginRegBg.animate().translationY(1570).setDuration(2200).setStartDelay(200);
-                loginComponent.animate().translationY(-300).alpha(0).setDuration(800).setStartDelay(800);
+                newMember();
             }
         });
         oldMember.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                newMemberLayout.animate().alpha(1).setDuration(1000).setStartDelay(1000);
-                oldMemberLayout.animate().alpha(0).setDuration(1000).setStartDelay(1000);
-                loginRegBg.animate().translationY(-1570).setDuration(2200).setStartDelay(200);
-                loginComponent.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(1300);
+                oldMember();
             }
         });
+    }
+
+    private void oldMember() {
+        newMemberLayout.animate().alpha(1).setDuration(1000).setStartDelay(1000);
+        oldMemberLayout.animate().alpha(0).setDuration(1000).setStartDelay(1000);
+        loginRegBg.animate().translationY(-1570).setDuration(2200).setStartDelay(200);
+        loginComponent.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(1300);
+        registerComponent.animate().translationY(0).alpha(0).setDuration(800).setStartDelay(800);
+    }
+
+    private void newMember() {
+        newMemberLayout.animate().alpha(0).setDuration(1000).setStartDelay(1000);
+        oldMemberLayout.animate().alpha(1).setDuration(1000).setStartDelay(1000);
+        loginRegBg.animate().translationY(1570).setDuration(2200).setStartDelay(200);
+        loginComponent.animate().translationY(-300).alpha(0).setDuration(800).setStartDelay(800);
+        registerComponent.animate().translationY(-180).alpha(1).setDuration(800).setStartDelay(1300);
     }
 }
