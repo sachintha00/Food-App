d2f9be5638fc09f8f34c14c01123dabca3055cc2
sachintha00 <sachintha0603@gmail.com>
2021-10-23 19:34:21 +0530

    Update AdminDrawerAdapter.java

--- App/app/src/main/java/com/smd/foodmaster/AdminAdapters/AdminDrawerAdapter.java
+++ App/app/src/main/java/com/smd/foodmaster/AdminAdapters/AdminDrawerAdapter.java
@@ -13,11 +13,11 @@ import java.util.Map;
 
 public class AdminDrawerAdapter extends RecyclerView.Adapter<AdminDrawerAdapter.ViewHolder> {
 
-    private List<AdminDrawerItem> items;
+    private List<AdminDrawerItem<ViewHolder>> items;
     private Map<Class<? extends AdminDrawerItem>,Integer>  viewType;
-    private SparseArray<AdminDrawerItem> holderFactories;
+    private SparseArray<AdminDrawerItem<ViewHolder>> holderFactories;
 
-    public AdminDrawerAdapter(List<AdminDrawerItem> items){
+    public AdminDrawerAdapter(List<AdminDrawerItem<ViewHolder>> items){
         this.items = items;
         this.viewType = new HashMap<>();
         this.holderFactories = new SparseArray<>();
@@ -25,24 +25,49 @@ public class AdminDrawerAdapter extends RecyclerView.Adapter<AdminDrawerAdapter.
     }
 
     private void processViewType() {
-
+        int type = 0;
+        for(AdminDrawerItem item : items){
+            if(!viewType.containsKey(item.getClass())){
+                viewType.put(item.getClass(),type);
+                holderFactories.put(type,item);
+                type++;
+            }
+        }
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         ViewHolder holder = holderFactories.get(viewType).createViewHolder(parent);
-        holder.
+        holder.adminDrawerAdapter = this;
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        items.get(position).bindViewHolder(holder);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return items.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return viewType.get(items.get(position).getClass());
+    }
+
+    public void setSelected(int position){
+        AdminDrawerItem newChecked = items.get(position);
+        if(!newChecked.isSelectable()){
+            return;
+        }
+
+        for(int i=0; i<items.size(); i++){
+            AdminDrawerItem item = items.get(i);
+            
+        }
     }
 
     static abstract class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
