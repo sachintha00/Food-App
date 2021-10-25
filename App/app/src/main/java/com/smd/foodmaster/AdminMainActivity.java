285f35f7b9b73916450527ea5e89a6a2c17459ad
sachintha00 <sachintha0603@gmail.com>
2021-10-25 21:44:07 +0530

    Update AdminMainActivity.java

--- App/app/src/main/java/com/smd/foodmaster/AdminMainActivity.java
+++ App/app/src/main/java/com/smd/foodmaster/AdminMainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class AdminMainActivity extends AppCompatActivity {
 
     public static final int POS_CLOSE = 0;
+    public static final int POS_DASHBOARD = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
