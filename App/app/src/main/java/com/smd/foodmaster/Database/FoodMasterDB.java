5a470d97db4cba6377592dba5c512df1e660076f
sachintha00 <sachintha0603@gmail.com>
2022-01-03 11:37:46 +0530

    create database class

---
+++ App/app/src/main/java/com/smd/foodmaster/Database/FoodMasterDB.java
@@ -0,0 +1,19 @@
+package com.smd.foodmaster.Database;
+
+import android.provider.BaseColumns;
+
+public final class FoodMasterDB {
+    // To prevent someone from accidentally instantiating the contract class,
+    // make the constructor private.
+    private FoodMasterDB() {}
+
+    /* Inner class that defines the table contents */
+    public static class Users implements BaseColumns {
+        public static final String TABLE_NAME = "UserDetails";
+        public static final String COLUMN_FIRST_NAME = "FirstName";
+        public static final String COLUMN_LAST_NAME = "LastName";
+        public static final String COLUMN_USER_ADDRESS = "Address";
+        public static final String COLUMN_USER_GMAIL = "Gmail";
+        public static final String COLUMN_USER_MOBILE = "MobileNumber";
+    }
+}
