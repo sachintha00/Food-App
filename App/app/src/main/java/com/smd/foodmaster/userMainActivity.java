7d2a3c8792bd00ce18f45574810d7ecca95d4748
sachintha00 <sachintha0603@gmail.com>
2022-02-07 16:07:52 +0530

    updated

--- App/app/src/main/java/com/smd/foodmaster/userMainActivity.java
+++ App/app/src/main/java/com/smd/foodmaster/userMainActivity.java
@@ -29,13 +29,13 @@ public class userMainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarUserMain.toolbar);
-        binding.appBarUserMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        binding.appBarUserMain.fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
