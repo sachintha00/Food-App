3f4525617b673a3bc7f41052cdf4368da3c2d97a
sachintha00 <sachintha0603@gmail.com>
2022-02-11 06:38:59 +0530

    updated

---
+++ App/app/src/main/java/com/smd/foodmaster/model/CategoryItem.java
@@ -0,0 +1,11 @@
+package com.smd.foodmaster.model;
+
+public class CategoryItem {
+    int itemId;
+    String imageUrl;
+
+    public CategoryItem(int itemId, String imageUrl) {
+        this.itemId = itemId;
+        this.imageUrl = imageUrl;
+    }
+}
